03d369e0d165d7345aa4b09de8e690c11cf39fec
Aleksa Majkic <aleksamcode@gmail.com>
2021-05-09 16:33:44 +0200

    Add project files.

---
+++ monte-carlo-pi-approximation/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace monte_carlo_pi_approximation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
