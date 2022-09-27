76670ad442915bb9b86b52a2301bbbe6944a08e2
AleksaMCode <aleksamcode@gmail.com>
2022-09-27 11:48:55 +0200

    Point class added

---
+++ monte-carlo-pi-approximation/Point.cs
@@ -0,0 +1,14 @@
+namespace monte_carlo_pi_approximation
+{
+    public class Point
+    {
+        public double PointX { get; set; }
+        public double PointY { get; set; }
+
+        public Point(double x, double y)
+        {
+            PointX = x;
+            PointY = y;
+        }
+    }
+}
