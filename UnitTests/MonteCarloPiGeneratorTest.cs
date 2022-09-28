54859830389b57cf94faa87cc5b0a637dcb01493
AleksaMCode <aleksamcode@gmail.com>
2022-09-28 03:44:31 +0200

    Unit MsTest tests added

---
+++ UnitTests/MonteCarloPiGeneratorTest.cs
@@ -0,0 +1,61 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using monte_carlo_pi_approximation;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MonteCarloPiGeneratorTest
+    {
+        private MonteCarloPiApproxGenerator gen;
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            gen = new MonteCarloPiApproxGenerator();
+            MonteCarloPiApproxGenerator.ScalingFactor = 1.0;
+
+            for (var i = 0; i < 1_000; ++i)
+            {
+                gen.GeneratePoint();
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(8.889, 2, 8.88)]
+        [DataRow(8.884, 2, 8.88)]
+        [DataRow(108.79892, 1, 108.7)]
+        public void TestFloor(double value, int decimal_places, double expected)
+        {
+            var actual = MonteCarloPiApproxGenerator.Floor(value, decimal_places);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPointCount()
+        {
+            MyTestInitialize();
+            Assert.AreEqual(gen.PointsInCircle.Count, gen.NumberOfPointsInsideCircle);
+            Assert.AreEqual(gen.PointsNotInCircle.Count, gen.NumberOfPoints - gen.NumberOfPointsInsideCircle);
+        }
+
+        [TestMethod]
+        public void TestPointsPlacement()
+        {
+            for (var i = 0; i < 100; ++i)
+            {
+                gen.GeneratePoint();
+                var point = gen.Point;
+
+                if (Math.Sqrt(point.X * point.X + point.Y * point.Y) <= 1.0)
+                {
+                    Assert.IsTrue(gen.PointsInCircle.Contains(gen.Point));
+                }
+                else
+                {
+                    Assert.IsTrue(gen.PointsNotInCircle.Contains(gen.Point));
+                }
+            }
+        }
+    }
+}
